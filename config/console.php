ea20b8e1955b89e0a746205105cadf724c274d36
vilintritenmert <vilintritenmert@gmail.com>
2017-01-05 03:26:49 +0200

    params

--- config/console.php
+++ config/console.php
@@ -1,10 +1,13 @@
 <?php
 
-$params = require(__DIR__ . '/params.php');
-$db = require(__DIR__ . '/db.php');
+if (file_exists(__DIR__ . '/params.php')) {
+    $params = require(__DIR__ . '/params.php');
+} else {
+    $params = null;
+}
 
-$config = [
-    'id' => 'basic-console',
+return [
+    'id' => 'minimal-console',
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
     'controllerNamespace' => 'app\commands',
@@ -31,13 +34,3 @@ $config = [
     ],
     */
 ];
-
-if (YII_ENV_DEV) {
-    // configuration adjustments for 'dev' environment
-    $config['bootstrap'][] = 'gii';
-    $config['modules']['gii'] = [
-        'class' => 'yii\gii\Module',
-    ];
-}
-
-return $config;
